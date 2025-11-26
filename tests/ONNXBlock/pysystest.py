fa6a2dcb421e52fda3921e23fee3ff2c8fe71b8e
Matthew Johnson <matthew.johnson@cumulocity.com>
2025-11-26 11:48:38 +0000

    Add ONNX block

---
+++ tests/ONNXBlock/pysystest.py
@@ -0,0 +1,44 @@
+#
+#  Copyright (c) 2025-present Cumulocity GmbH, Duesseldorf, Germany and/or its affiliates and/or their licensors.
+#   This file is licensed under the Apache 2.0 license - see https://www.apache.org/licenses/LICENSE-2.0
+#
+
+__pysys_title__ = r'ONNX block: to check the basic working of the block'
+__pysys_purpose__ = r''
+
+from pysys.constants import *
+from apamax.analyticsbuilder.basetest import AnalyticsBuilderBaseTest
+
+class PySysTest(AnalyticsBuilderBaseTest):
+
+	def preInjectBlock(self, corr):
+		self._injectEPLOnce(corr, [self.project.APAMA_HOME+'/monitors/'+i+'.mon' for i in ['Base64']])  
+		self._injectEPLOnce(corr, [self.project.testRootDir+'/utils/DeviceServiceMock.mon'])
+
+	def execute(self):
+		self.correlator = self.startAnalyticsBuilderCorrelator(blockSourceDir=f'{self.project.SOURCE}/blocks/', arguments=["--config", f"{self.project.SOURCE}/blocks/ONNX/plugin.yaml", "--config", f"{self.project.SOURCE}/blocks/Python/plugin.yaml"])
+		
+		self.modelId = self.createTestModel('apamax.analyticsbuilder.samples.ONNX', {
+			'onnxModelFile': 'add.onnx',
+		})
+		
+		self.sendEventStrings(self.correlator,
+								self.timestamp(1),
+								self.inputEvent('value1', 12.25, id = self.modelId, properties={'value1': 'value'}),
+								self.timestamp(2),
+								self.inputEvent('value2', 7.75, id = self.modelId, properties={'value2': 'value'}),
+								self.timestamp(2.1),
+								self.inputEvent('value2', 17.25, id=self.modelId, properties={'value2': 'value'}),
+								self.timestamp(2.5),
+							  )
+
+
+	def validate(self):
+		# Verifying that there are no errors in log file.
+		self.checkLogs(errorIgnores=['Unknown dynamicChain', 'CumulocityRestAPIMonitor', 'CumulocityRequestInterface', 'Notifications2Subscriber'])
+		
+		# Verifying that the model is deployed successfully.
+		self.assertGrep(self.analyticsBuilderCorrelator.logfile, expr='Model \"' + self.modelId + '\" with PRODUCTION mode has started')
+		
+		# Verifying the result - output from the block.
+		self.assertBlockOutput('result1', [20, 29.5])
